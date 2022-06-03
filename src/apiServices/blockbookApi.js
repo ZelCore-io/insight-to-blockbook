2e420d38af47fa5576a605064ba26a43f494fb32
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2022-06-03 14:34:47 +0700

    add tx list, addr info, send tx

--- src/apiServices/blockbookApi.js
+++ src/apiServices/blockbookApi.js
@@ -66,7 +66,137 @@ async function getUtxos(req, res) {
   }
 }
 
+async function getAddr(req, res) {
+  try {
+    let { address } = req.params;
+    address = address || req.query.address;
+    let { noTxList } = req.params;
+    noTxList = noTxList || req.query.noTxList;
+    console.log(noTxList);
+    // ignore noTxList
+    const url = `${config.blockbook}/api/v2/address/${address}?details=basic`;
+
+    const response = await axios.get(url);
+    const urlResponse = response.data;
+
+    const decimals = 8;
+
+    const myResponse = {
+      addrStr: urlResponse.address,
+      balance: Number((+urlResponse.balance / (10 ** decimals)).toFixed(8)),
+      balanceSat: urlResponse.balance,
+      totalReceived: Number((+urlResponse.totalReceived / (10 ** decimals)).toFixed(8)),
+      totalReceivedSat: +urlResponse.totalReceived,
+      totalSent: Number((+urlResponse.totalSent / (10 ** decimals)).toFixed(8)),
+      totalSentSat: +urlResponse.totalSent,
+      unconfirmedBalance: Number((+urlResponse.unconfirmedBalance / (10 ** decimals)).toFixed(8)),
+      unconfirmedBalanceSat: +urlResponse.unconfirmedBalance,
+      unconfirmedTxApperances: urlResponse.unconfirmedTxs,
+      txApperances: urlResponse.txs,
+    };
+    res.json(myResponse);
+  } catch (error) {
+    log.error(error);
+    res.statusCode = 500;
+    res.json({
+      status: 'error',
+      error,
+    });
+  }
+}
+
+async function getTxs(req, res) {
+  try {
+    let { address } = req.params;
+    address = address || req.query.address;
+    let { from } = req.params;
+    from = from || req.query.from;
+    let { to } = req.params;
+    to = to || req.query.to;
+    let url = `${config.blockbook}/api/v2/address/${address}?pageSize=50&details=txs`;
+    // eslint-disable-next-line eqeqeq
+    if (from != 0) {
+      url = `${config.blockbook}/api/v2/address/${address}?pageSize=1000&details=txs`;
+    }
+
+    const response = await axios.get(url);
+    const urlResponse = response.data;
+
+    const myResponse = {
+      totalItems: urlResponse.txs,
+      from,
+      to: urlResponse.itemsOnPage > to ? urlResponse.itemsOnPage : to,
+      items: [],
+    };
+
+    const decimals = 8;
+
+    urlResponse.transnactions.forEach((tx) => {
+      // eslint-disable-next-line no-param-reassign
+      tx.time = tx.blockTime;
+      // eslint-disable-next-line no-param-reassign
+      tx.fees = Number((+tx.fees / (10 ** decimals)).toFixed(8));
+      tx.vin.forEach((vin) => {
+        // eslint-disable-next-line no-param-reassign
+        vin.value = Number((+vin.value / (10 ** decimals)).toFixed(8));
+      });
+      tx.vout.forEach((vout) => {
+        // eslint-disable-next-line no-param-reassign
+        vout.value = Number((+vout.value / (10 ** decimals)).toFixed(8));
+      });
+      myResponse.items.push(tx);
+    });
+
+    const responseString = JSON.stringify(myResponse);
+    const q = responseString.replaceAll('OP_RETURN (', 'OP_RETURN ');
+    res.json(JSON.parse(q));
+  } catch (error) {
+    log.error(error);
+    res.statusCode = 500;
+    res.json({
+      status: 'error',
+      error,
+    });
+  }
+}
+
+async function sendTx(req, res) {
+  let body = '';
+  req.on('data', (data) => {
+    body += data;
+  });
+  req.on('end', async () => {
+    try {
+      if (typeof body !== 'object') {
+        body = JSON.parse(body);
+      }
+      const hex = body.rawtx;
+      const url = `${config.blockbook}/api/v2/sendtx/`;
+
+      const response = await axios.post(url, hex);
+      const urlResponse = response.data;
+
+      const txid = urlResponse.result;
+
+      const myResponse = {
+        txid,
+      };
+      res.json(myResponse);
+    } catch (error) {
+      log.error(error);
+      res.statusCode = 500;
+      res.json({
+        status: 'error',
+        error,
+      });
+    }
+  });
+}
+
 module.exports = {
   getStatus,
   getUtxos,
+  getAddr,
+  getTxs,
+  sendTx,
 };
--- src/routes.js
+++ src/routes.js
@@ -7,4 +7,13 @@ module.exports = (app) => {
   app.get('/api/addr/:address?/utxo', (req, res) => {
     blockbookApi.getUtxos(req, res);
   });
+  app.get('/api/addr/:address?/:noTxList?', (req, res) => {
+    blockbookApi.getAddr(req, res);
+  });
+  app.get('/api/addrs/:address?/txs/:from?/:to?', (req, res) => {
+    blockbookApi.getTxs(req, res);
+  });
+  app.post('/api/tx/send', (req, res) => {
+    blockbookApi.sendTx(req, res);
+  });
 };
